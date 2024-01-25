bbf0b9b11622f8f2dbef4b7c653fd9a3b8392675
vaguue <little_scamp@yahoo.com>
2024-01-25 07:49:15 +0300

    checksum of array

--- cxx/checksums/Checksums.cpp
+++ cxx/checksums/Checksums.cpp
@@ -7,11 +7,27 @@ Napi::Object Init(Napi::Env env, Napi::Object exports) {
   return exports;
 }
 
+pcpp::ScalarBuffer<uint16_t> convertBuf(const Napi::Value& val) {
+  Napi::Buffer<uint16_t> buf = val.As<Napi::Buffer<uint16_t>>();
+  return { buf.Data(), buf.Length() };
+}
+
 Napi::Value IPChecksum(const Napi::CallbackInfo& info) {
   checkLength(info, 1);
-  Napi::Buffer<uint16_t> buf = info[0].As<Napi::Buffer<uint16_t>>();
-  pcpp::ScalarBuffer<uint16_t> scalarBuf = { buf.Data(), buf.Length() };
-  return Napi::Number::New(info.Env(), ntohs(computeChecksum(&scalarBuf, 1)));
+
+  std::vector<pcpp::ScalarBuffer<uint16_t>> bufs;
+
+  if (info[0].IsArray()) {
+    Napi::Array ar = info[0].As<Napi::Array>();
+    for (size_t i{}; i < ar.Length(); ++i) {
+      bufs.push_back(convertBuf(ar[i]));
+    }
+  }
+  else {
+    bufs.push_back(convertBuf(info[0]));
+  }
+
+  return Napi::Number::New(info.Env(), ntohs(computeChecksum(bufs.data(), bufs.size())));
 }
 
 }
