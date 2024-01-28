bc892a9d57cee304abcdaee2f78de7861b1ec344
vaguue <little_scamp@yahoo.com>
2024-01-28 16:05:55 +0300

    tests

---
+++ test/Ethernet.test.js
@@ -0,0 +1,11 @@
+const { strict: assert } = require('node:assert');
+const test = require('node:test');
+
+const { extendAt, shrinkAt } = require('#lib/buffer');
+const { Ethernet } = require('#lib/layers/Ethernet');
+
+test('Ethernet', async (t) => {
+  const eth = new Ethernet(Buffer.from('00ebd8f4bbe75ce91e9dfc40080045000073000040', 'hex'));
+  eth.src = eth.dst;
+  assert.deepEqual(eth.toObject(), { dst: '00:eb:d8:f4:bb:e7', src: '00:eb:d8:f4:bb:e7', type: 2048 });
+});
---
+++ test/IPv4.test.js
@@ -0,0 +1,67 @@
+const { strict: assert } = require('node:assert');
+const test = require('node:test');
+
+const { extendAt, shrinkAt } = require('#lib/buffer');
+const { IPv4 } = require('#lib/layers/IPv4');
+
+test('IPv4', async (t) => {
+  const buf = Buffer.from('450000730000400040068cd2c0a80167cebd1ce6e33d5debb394ef8ddd9ed0568018080', 'hex');
+  const ip = new IPv4(buf, {
+    shrinkAt(...args) {
+      return shrinkAt(buf, ...args);
+    },
+    extendAt(...args) {
+      return extendAt(buf, ...args);
+    },
+  });
+
+  assert.deepEqual(
+    ip.toObject(),
+    {
+      headerLength: 5,
+      version: 4,
+      typeOfService: 0,
+      totalLength: 115,
+      id: 0,
+      fragmentOffsetRaw: 64,
+      timeToLive: 64,
+      protocol: 6,
+      checksum: 36050,
+      src: '192.168.1.103',
+      dst: '206.189.28.230',
+      fragmentInfo: { isFragment: false, value: 0, flags: 64 }
+    }
+  );
+
+  assert.equal(ip.length, 20);
+
+  const { checksum } = ip;
+  ip.checksum = 0;
+  ip.calculateChecksum();
+  assert.equal(ip.checksum, checksum);
+
+  ip.dst = '192.168.1.1';
+  assert.equal(ip.dst, '192.168.1.1');
+
+  let options = [
+    { type: 1, recLength: 4, value: Buffer.from([0xaa, 0xaa, 0xaa, 0xaa]) },
+    { type: 2, recLength: 2, value: Buffer.from([0xbb, 0xbb]) },
+    { type: 0, recLength: 0, value: Buffer.from([]) }
+  ];
+
+  ip.options = options;
+
+  assert.deepEqual([...ip.options], options);
+  assert.equal(ip.length, 32);
+
+  options = [
+    { type: 1, recLength: 4, value: Buffer.from([0xaa, 0xaa, 0xaa, 0xaa]) },
+    { type: 0, recLength: 0, value: Buffer.from([]) }
+  ];
+
+  ip.options = options;
+
+  assert.deepEqual([...ip.options], options);
+
+  assert.deepEqual(ip.toObject(), new IPv4(ip.toObject()).toObject());
+});
